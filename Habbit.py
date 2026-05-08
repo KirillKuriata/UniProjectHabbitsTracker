94b1537c1d6184cfc1500a776abbf25b19a241c2
Kiril <kkirill2121@gmail.com>
2026-05-08 10:59:15 +0200

    .

--- Habbit.py
+++ Habbit.py
@@ -3,9 +3,10 @@ from collections import namedtuple
 
 class Habbit:
 
-    def __init__(self, calendarOfCompleting, name):
+    def __init__(self, calendarOfCompleting, name, descripition):
         self.calendarOfCompleting = dict(calendarOfCompleting)
         self.name = name
+        self.description = descripition
 
 
-    def setCompleted():
+    def setCompleted(s):
